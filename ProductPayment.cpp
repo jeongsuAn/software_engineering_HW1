6a8b454d653759a91969564359e57f200edea9d7
jeongsu <somajeongsu@gmail.com>
2022-06-03 05:22:02 +0900

    Delete unnecessary source code

--- ProductPayment.cpp
+++
@@ -1,8 +0,0 @@
-#include "ProductPayment.h"
-
-productPayment::productPayment(FILE *out_fp, Product p)
-{
-    PurchaseHistoryCollection PHC;
-    PHC.addPurchaseHistory(p);
-    productPaymentUI ppUI(out_fp, p);
-}
